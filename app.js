d8954895233895691ea3a0356c36ff4ad11fbf12
Matt Haines <mhpahuta@gmail.com>
2022-02-25 10:41:59 -0800

    Add dynamic unit entry functionality, form input

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-// save number to variable
-let num = 20;
+// save number to variable - initial static num for testing
+// let num = 0;
 
 const unitEntryVal = document.getElementById('unit-entry-val');
 const metricInitialVals = document.querySelectorAll('.metric-initial');
 const impInitialVals = document.querySelectorAll('.imp-initial');
 
+const form = document.querySelector('form');
+const input = document.querySelector('#unit-input');
 const calcButton = document.getElementById('calculate-btn');
 
 // results elements
@@ -14,29 +16,6 @@ const gallonResults = document.getElementById('gallon-results');
 const literResults = document.getElementById('liter-results');
 const poundResults = document.getElementById('pound-results');
 const kiloResults = document.getElementById('kilo-results');
-
-// *** calculations/formulas *** //
-// ***************************** //
-/* 
-  meters to feet 
-  One meter is approximately equal to 3.28084 feet. To convert meters to feet, multiply the given meter value by 3.28084 feet
-  - num * 3.28084 
-
-  liters to gallons
-  gallons = 0.264172 × liters. Therefore, we just multiply the liters by 0.264172 or divide by 3.785 to convert them into gallons.
-
-  kilograms to pounds 
-  The approximation we use for kilograms (kg) to pounds (lb) is 1 kg = 2.2 lb. To convert from kilogram to pound, we multiply by 2.2.
-
-*/
-
-// set initial values
-unitEntryVal.textContent = num;
-
-function setInitialVals() {
-  metricInitialVals.forEach(val => val.textContent = num)
-  impInitialVals.forEach(val => val.textContent = num)
-}
 
 // Calculation functions
 function metersToFeet(meters) {
@@ -63,23 +42,27 @@ function poundsToKilograms(pounds) {
   return (pounds / 2.2046).toFixed(3);
 }
 
-setInitialVals();
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const valToConvert = Number(input.value);
+  renderResults(valToConvert);
+  setConvertVals(valToConvert);
+  form.reset();
+})
 
-// calcButton.addEventListener('click', () => {
-//   feetResults.textContent = metersToFeet(num);
-//   meterResults.textContent = feetToMeters(num);
-//   gallonResults.textContent = litersToGallons(num);
-//   literResults.textContent = gallonsToLiters(num);
-//   poundResults.textContent = kilogramsToPounds(num);
-//   kiloResults.textContent = poundsToKilograms(num);
-// })
+function setConvertVals(num) {
+  metricInitialVals.forEach(val => val.textContent = num)
+  impInitialVals.forEach(val => val.textContent = num)
+}
+
+function renderResults(num) {
+  feetResults.textContent = metersToFeet(num);
+  meterResults.textContent = feetToMeters(num);
+  gallonResults.textContent = litersToGallons(num);
+  literResults.textContent = gallonsToLiters(num);
+  poundResults.textContent = kilogramsToPounds(num);
+  kiloResults.textContent = poundsToKilograms(num);
+}
 
 
-
-feetResults.textContent = metersToFeet(num);
-meterResults.textContent = feetToMeters(num);
-gallonResults.textContent = litersToGallons(num);
-literResults.textContent = gallonsToLiters(num);
-poundResults.textContent = kilogramsToPounds(num);
-kiloResults.textContent = poundsToKilograms(num);
 
